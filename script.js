171de2316c91fece02c7749b6ac33a0a2660e911
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-07-13 23:14:06 -0500

    added 'Sensee' class and used inheritance/super to access functions from the parent 'Ninja'.

--- script.js
+++ script.js
@@ -1,23 +1,42 @@
 
 class Ninja {
-    constructor(name, health=0, speed = 3, strength = 3){
-        this.name = name;
-        this.health = health;
-        this.speed = speed;
-        this.strength = strength;
+    constructor(name, health = 0, speed = 3, strength = 3) {
+      this.name = name;
+      this.health = health;
+      this.speed = speed;
+      this.strength = strength;
     }
-    sayName(name){
-        console.log("My name is ", this.name);
+    sayName(name) {
+      console.log("My name is ", this.name);
+      return this;
     }
-    showStats(name, strength, speed, health){
-        console.log("Name :", this.name, ", Health: ", this.health, ", Strength :", this.strength, ", Speed :", this.speed);
+    showStats(name, strength, speed, health) {
+      console.log("Name :", this.name, ", Health: ", this.health, ", Strength :", this.strength, ", Speed :", this.speed);
+      return this;
     }
-    drinkSake(){
-        this.health += 10;
+    drinkSake() {
+      this.health += 10;
+      return this;
     }
-}
-
-const ninja1 = new Ninja("Hyabusa");
-ninja1.sayName();
-ninja1.drinkSake();
-ninja1.showStats();
+  }
+  
+  class Sensei extends Ninja {
+    constructor(name, wisdom=10) {
+      super(name, 200, 10, 10);
+      this.wisdom = wisdom;
+    }
+    speakWisdom() {
+      super.drinkSake();
+      console.log("What one programmer can do in one month, two programmers can do in two months.")
+    }
+    showStats() {
+      super.showStats();
+      console.log("Wisdom :", this.wisdom);
+    }
+  }
+  
+  const genkai = new Sensei("Genkai")
+  
+  genkai.speakWisdom();
+  genkai.showStats();
+  
